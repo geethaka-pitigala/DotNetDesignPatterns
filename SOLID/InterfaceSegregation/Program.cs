a0b572768b62237c0698820f7f8f43c652859aa5
Geethaka Pitigala <66256789+geethaka-pitigala@users.noreply.github.com>
2025-03-30 15:01:01 +0530

    SOLID | Interface Segregation

---
+++ SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace InterfaceSegregation{
+public class Program{
+    static void Main(string[] args){
+        Photocopier photocopier = new Photocopier();
+        Document document = new Document("SOLID Principles", 300);
+        photocopier.Print(document);
+        photocopier.Scan(document);
+
+    }
+
+}
+
+public class Document{
+    public string Name { get; set; }
+    public int Pages { get; set; }
+
+    public Document(string name, int pages)
+    {
+        this.Name = name;
+        this.Pages = pages;
+    }
+
+    public override string ToString(){
+        return $"Document {Name} has {Pages} pages";
+    }
+}
+
+public interface IScanner{
+    public void Scan(Document document);
+}
+
+public interface IPrinter{
+    public void Print(Document document);
+}
+
+public class Photocopier: IScanner, IPrinter{
+    public void Scan(Document document){
+        Console.WriteLine($"{document.ToString()} is scanned.");
+    }
+
+    public void Print(Document document){
+        Console.WriteLine($"{document.ToString()} is printed.");
+    }
+}
+
+}
