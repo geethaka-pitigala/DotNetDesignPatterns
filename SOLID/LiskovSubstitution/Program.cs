96168db3e2265adcfd21d13a6841ff877daeb602
Geethaka Pitigala <66256789+geethaka-pitigala@users.noreply.github.com>
2025-03-30 11:24:22 +0530

    Apply new .gitignore rules

---
+++ SOLID/LiskovSubstitution/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Liskov;
+public class Liskov {
+    static void Main(string[] args){
+        Rectangle square = new Square();
+        square.Height = 4;
+
+        Console.WriteLine(square.toString());
+    }
+
+}
+
+public class Rectangle{
+    public virtual int Height { get; set; }
+    public virtual int Width { get; set; }
+
+    public Rectangle(){}
+
+    public Rectangle(int height, int width){
+        this.Height = height;
+        this.Width = width;
+    }
+
+    public int area(){
+        return this.Height * this.Width;
+    }
+
+    public string toString(){
+        return $"A rectangle with Height {Height} and Width {Width} with Area {area()}";
+    }
+
+}
+
+public class Square: Rectangle{
+    public Square(){}
+
+    public Square(int sideLength)
+    {
+        base.Height = base.Width = sideLength;
+    }
+
+    public override int Width { 
+        set{
+            base.Height = base.Width = value;
+        }
+     }
+    public override int Height { 
+        set{
+            base.Height = base.Width = value;
+        }
+    }
+
+}
