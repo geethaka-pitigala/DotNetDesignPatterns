d3c602b181e97e9636fe82551fd2cab649652b7c
Geethaka Pitigala <66256789+geethaka-pitigala@users.noreply.github.com>
2025-04-25 21:58:08 +0530

    Creational Design Patterns | Factory | Inner Factory

---
+++ Creational Design Patterns/Factory/InnerFactory/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CreationalDesignPatterns.Factory.InnerFactory
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            var cartesianPoint = Point.Factory.CartisianPoint(3, 4);
+            var polarPoint = Point.Factory.PolarPoint(5, Math.PI / 4);
+
+            Console.WriteLine(cartesianPoint); // Output: Point(3, 4)
+            Console.WriteLine(polarPoint); // Output: Point(3, 3)
+        }
+    }
+
+}
